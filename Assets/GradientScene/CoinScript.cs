4b90d81f46a855b73267388b762e1039a5f33e3c
Eric Howard <howard.eric.m@gmail.com>
2015-11-01 13:35:01 -0800

    added loot scene

---
+++ Assets/GradientScene/CoinScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinScript : MonoBehaviour {
+	Rigidbody _rigidbody;
+	// Use this for initialization
+	void Start () {
+		_rigidbody = gameObject.GetComponent<Rigidbody>();
+		_rigidbody.AddForceAtPosition(Vector3.up * Random.Range( 400f,900f), Random.insideUnitSphere * 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		transform.Rotate(0,5,0);
+	}
+}
---
+++ Assets/SpawnerScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerScript : MonoBehaviour {
+
+	public Transform Prefab;
+	public int SpawnPerSecond = 2;
+	public float Delay = 1f;
+	public float StopAt = 5f;
+
+	float _timeSpawning = 0f;
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("Spawn", Delay);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Spawn() {
+		_timeSpawning += (1f / SpawnPerSecond);
+		Instantiate(Prefab, transform.position,Quaternion.identity);
+
+		if(_timeSpawning < StopAt)
+			Invoke ("Spawn", 1f / SpawnPerSecond);
+	}
+}
